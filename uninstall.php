bd17e47eacb411e4136ae9ab14c86a9584470f01
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2021-04-29 08:35:42 +0900

    [fix] Missing reading of required files

--- uninstall.php
+++ uninstall.php
@@ -12,6 +12,8 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-tidy-head-tag-data.php';
+
 foreach ( Tidy_Head_Tag_Data::DB_NAME as $db_name_key => $db_name_value ) {
 	if ( isset( $db_name_value ) ) {
 		delete_option( $db_name_value );
