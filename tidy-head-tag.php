29637ab4ba38b5c9224aad4d64905f6177646e0f
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2021-05-01 10:39:03 +0900

    [update] Version number

--- tidy-head-tag.php
+++ tidy-head-tag.php
@@ -35,7 +35,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'TIDY_HEAD_TAG_VERSION', '1.0.0' );
+define( 'TIDY_HEAD_TAG_VERSION', '1.1.0' );
 
 /**
  *  Include core plugin class file.
